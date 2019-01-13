9e7cd5286bc8610d5f65b026955a38bf149e7ce4
Ilya Koziev <inkoziev@gmail.com>
2019-01-13 14:18:18 +0300

    Added Travis CI job file

---
+++ rusyllab/tests.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from rusyllab import split
+
+
+def word2pieces(word):
+    return '|'.join(split(word))
+
+
+def test_split_word():
+    assert(word2pieces(u'кошка') == u'кош|ка')
+
+
+if __name__ == '__main__':
+    test_split_word()
